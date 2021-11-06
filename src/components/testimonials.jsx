dcc00c9554e1ec7c4820d56c9be70a0b6051bc7d
biswajeet619 <biswajeet15696@gmail.com>
2021-11-06 13:12:39 +0530

    first commit

---
+++ src/components/testimonials.jsx
@@ -0,0 +1,28 @@
+export const Testimonials = (props) => {
+  return (
+    <div id='testimonials'>
+      <div className='container'>
+        <div className='section-title text-center'>
+          <h2>Key Locations we operate in Hyderabad</h2>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  <div className='testimonial'>
+                    <div className='testimonial-image'>
+                      {' '}
+                      <img src={d.img} alt='' />{' '}
+                    </div>  
+                    <div className='testimonial-content'>
+                      <div className='testimonial-meta'> {d.name} </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
